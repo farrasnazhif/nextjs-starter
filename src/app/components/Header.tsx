7dd6414b585c4acbe57184463e986b88ad9d29a4
farrasnazhif <farrasnazhif@gmail.com>
2025-11-16 20:38:10 +0700

    feat: dark mode

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import {
+  COLOR_THEME_COOKIE_NAME,
+  DARK_TOKENS,
+  LIGHT_TOKENS,
+  WEB_NAME,
+} from "@/constants";
+import React from "react";
+import { Sun, Moon } from "lucide-react";
+import Cookie from "js-cookie";
+
+export default function Header({ initialTheme }: { initialTheme: string }) {
+  const [theme, setTheme] = React.useState(initialTheme);
+
+  function handleToggleTheme() {
+    const newTheme = theme === "light" ? "dark" : "light";
+
+    setTheme(newTheme);
+
+    Cookie.set(COLOR_THEME_COOKIE_NAME, newTheme, {
+      expires: 1000,
+    });
+
+    const newTokens = newTheme === "light" ? LIGHT_TOKENS : DARK_TOKENS;
+    const root = document.documentElement;
+
+    root.setAttribute("data-color-theme", newTheme);
+    Object.entries(newTokens).forEach(([key, value]) => {
+      root.style.setProperty(key, value);
+    });
+  }
+
+  return (
+    <header className="w-full flex items-center justify-between px-6 py-4 shadow ">
+      <h1 className="text-2xl font-bold ">{WEB_NAME}</h1>
+      <button onClick={handleToggleTheme} className="px-4 py-2 border rounded ">
+        {theme === "light" ? <Moon /> : <Sun />}
+      </button>
+    </header>
+  );
+}
---
+++ src/constants.ts
@@ -0,0 +1,16 @@
+export const WEB_NAME = "NextJs Starter";
+export const COLOR_THEME_COOKIE_NAME = "color-theme";
+
+export const LIGHT_TOKENS: Record<string, string> = {
+  "--color-bg": "#ffffff",
+  "--color-text": "#171717",
+  "--color-primary": "#0ea5e9",
+  "--color-secondary": "#9333ea",
+};
+
+export const DARK_TOKENS: Record<string, string> = {
+  "--color-bg": "#0a0a0a",
+  "--color-text": "#f5f5f5",
+  "--color-primary": "#38bdf8",
+  "--color-secondary": "#c084fc",
+};
